fc2e59a5adce14bfe6b2ffb3fbf46450f6488ff2
wharleyornelasjs <wharleyornelasjs@gmail.com>
2018-09-26 16:56:05 -0300

    Refactored Main component.

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 import List from '../../styleguide/List/List'
 import ListItem from '../../styleguide/List/ListItem'
+import Content from '../../styleguide/Content/Content'
 
 const listItem = [
   { description: 'Presidente' },
@@ -12,9 +13,14 @@ const listItem = [
 ]
 
 const Main = () => (
-  <List>
-    <ListItem listItem={ listItem } />
-  </List>
+  <Fragment>
+    <List>
+      { listItem.map(item => (
+        <ListItem key={ item.description } description={ item.description } />
+      )) }
+    </List>
+    <Content />
+  </Fragment>
 )
 
 export default Main
