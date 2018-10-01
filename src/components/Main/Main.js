8036a3041b1fcbf1e9c3490a607dff448185405b
wharleyornelasjs <wharleyornelasjs@gmail.com>
2018-10-01 15:56:02 -0300

    Updated Main to add candidate.

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,8 +1,10 @@
 import React, { Fragment } from 'react'
+import { array } from 'prop-types'
 
 import List from '../../styleguide/List/List'
 import ListItem from '../../styleguide/List/ListItem'
 import Content from '../../styleguide/Content/Content'
+import Candidate from '../../styleguide/Candidate/Candidate'
 
 const listItem = [
   { description: 'Presidente' },
@@ -12,15 +14,23 @@ const listItem = [
   { description: 'Estadual' }
 ]
 
-const Main = () => (
+const Main = ({ candidate }) => (
   <Fragment>
     <List>
       { listItem.map(item => (
         <ListItem key={ item.description } description={ item.description } />
       )) }
     </List>
-    <Content />
+    <Content>
+      { candidate.map(data => (
+        <Candidate key={ data.id } name={ data.nomeUrna } number={ data.numero } />
+      )) }
+    </Content>
   </Fragment>
 )
+
+Main.propTypes = {
+  candidate: array
+}
 
 export default Main
